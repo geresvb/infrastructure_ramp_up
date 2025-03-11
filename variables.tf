787a409a09e31297b024374a4afc04e429f1c4bd
geresvb <german.valdes.bernal19@gmail.com>
2025-03-11 18:45:06 +0000

    Commentend

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
-#export= TF_VAR_VARIABLE="VALUE"
+#export TF_VAR_VARIABLE="VALUE"
 variable "subid" {
     description="Terraform environment variable to subscription_id"
     type=string
 }
+#export TF_VAR_subid=
 
 variable "tenid" {
     description="Terraform environment variable to tenant_id"
     type=string
 }
+#export TF_VAR_tenid=
