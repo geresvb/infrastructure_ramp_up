f7373a855f296698eb4d718bf404d96b6bcba03f
geresvb <german.valdes.bernal19@gmail.com>
2025-03-11 18:28:14 +0000

    Credentials as env

---
+++ variables.tf
@@ -0,0 +1,10 @@
+#export= TF_VAR_VARIABLE="VALUE"
+variable "subid" {
+    description="Terraform environment variable to subscription_id"
+    type=string
+}
+
+variable "tenid" {
+    description="Terraform environment variable to tenant_id"
+    type=string
+}
