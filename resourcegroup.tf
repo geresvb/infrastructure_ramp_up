65e0f132bd8dd0ffee00fcec3df4a09bb0214e7e
geresvb <german.valdes.bernal19@gmail.com>
2025-03-11 17:07:49 +0000

    Resource group name changed

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "RG_1" {
-  name     = "Terraform_Infrastructure_1"
+  name     = "Terraform_Infrastructure_test"
   location = "East US"
   tags = {
     Tower   = "Wintel"
