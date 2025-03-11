1af47063e2333309a99717edcbdbb5d08e944a92
geresvb <german.valdes.bernal19@gmail.com>
2025-03-11 18:07:20 +0000

    env az ids

--- providers.tf
+++ providers.tf
@@ -10,6 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "15158422-ef98-425f-a0e3-6e53c81ba478"
-  tenant_id       = "0b3fc178-b730-4e8b-9843-e81259237b77"
+  subscription_id = var.subid
+  tenant_id       = var.tenid
 }
